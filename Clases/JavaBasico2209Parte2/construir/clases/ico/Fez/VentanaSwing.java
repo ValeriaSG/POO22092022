fa199500dc262a3e169879c5a3c56ae61fe63a9f
ValeriaSG <91902764+ValeriaSG@users.noreply.github.com>
2022-05-17 09:07:26 -0500

    Ventana Swing
    Se actualizo en neatbeans

---
+++ Clases/JavaBasico2209Parte2/construir/clases/ico/Fez/VentanaSwing.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes.swing;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author valeria
+ */
+public class VentanaSwing extends JFrame {
+
+    private JTextField cuadro;
+    private JButton boton;
+    private JLabel resultado;
+    
+    public VentanaSwing() throws HeadlessException {
+        //configuracion de  ventana sea visible
+        setTitle("Conversión de °C  a  °F");
+        setSize(400, 300);
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        cuadro = new JTextField (5);
+        //insertar imagen
+        ImageIcon icono = new ImageIcon(System.getProperty("user.dir")+ "\\src\\ico\\fes\\swing\\Carro.png");
+        boton = new JButton (icono);
+       // boton = new JButton ("Convertir");
+            boton.setBackground(Color.GREEN);
+            boton.setOpaque(true);
+            //con texto
+            boton.setToolTipText("Click para convertir en °F");
+        resultado =  new JLabel(" °F ");
+        this.getContentPane().add(cuadro);
+        this.getContentPane().add(boton);
+        this.getContentPane().add(resultado);
+        this.validate();
+        this.setVisible(true);
+        
+        this.addWindowListener ( new  WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        
+        this.boton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                float gradosFarenheit=0.0f;
+                float celcius = 0.0f;
+                    try {
+                        
+                        celcius = Float.parseFloat(cuadro.getText());
+                        gradosFarenheit= (celcius * (9.0f/5.0f))+32.0f;
+                        resultado.setText(gradosFarenheit+" °F");
+                                
+                    } catch (Exception ex) {
+                        System.out.println(ex.toString());
+                        JOptionPane.showMessageDialog(null, "Introduce un valor correcto", "Error de Captura",JOptionPane.ERROR_MESSAGE );
+                    }
+            }
+           
+        });
+    }
+    
+}
